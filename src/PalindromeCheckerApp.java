8cb2b118ade7a02e8ad5c5b7f4a7cceaa75fb86e
Jaya2606 <js2606@srmist.edu.in>
2026-02-20 23:57:54 +0530

    UC2: Print hardcoded palindrome result

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -3,10 +3,18 @@ public class PalindromeCheckerApp {
     public static void main(String[] args) {
 
         System.out.println("Welcome to the Palindrome Checker Management System");
-
         System.out.println("Version : 1.0");
+        System.out.println("System initialized successfully.\n");
 
-        System.out.println("System initialized successfully.");
+        // UC2: Hardcoded Palindrome Result
+        String word = "madam";
+        String reversed = "madam";
+
+        if(word.equals(reversed)) {
+            System.out.println(word + " is a palindrome.");
+        } else {
+            System.out.println(word + " is not a palindrome.");
+        }
 
     }
 }
